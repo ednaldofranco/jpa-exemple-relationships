aa6406fc23bc6429e0b61d964b24f090b0c64c3c
ednaldo <francoednaldo.gyn@gmail.com>
2019-06-19 16:50:21 -0300

    criado repositorio de pessoa

---
+++ src/main/java/br/com/jpa/exemple/jpaexemple/repository/PessoaRepository.java
@@ -0,0 +1,6 @@
+package br.com.jpa.exemple.jpaexemple.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<PessoaRepository, Long> {
+}
